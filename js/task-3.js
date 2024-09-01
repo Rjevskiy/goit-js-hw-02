3a74f6576ed15f60eb8c23e313eadf91e95cf87f
Alexsander Rjevskiy <porezkakir2@gmail.com>
2024-09-01 18:57:36 +0300

    js-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,24 @@
-// function getElementWidth( content , padding , border  ) {
-    
-//     Npx = Number.parseInt (content) + (Number.parseInt(padding) *2)+ (Number.parseFloat(border) *2)
+function checkForSpam(message) {
+    const spam1 = "spam";
+    const spam2 = "sale";
+
+    const normMessage = message.toLowerCase();
+  
+    if (normMessage.includes(spam1))
+    {return true; }
+
+    else if(normMessage.includes(spam2))
+        {return true; }
+    else
+   {return false}
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
 
-//     return Npx 
-// }
-
-
-// console.log(getElementWidth("50px", "8px", "4px")); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
